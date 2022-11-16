6ca6c29948273cacf99d0a11d515898f6d49c86e
Olga <svey699@gmail.com>
2022-11-16 21:32:31 +0500

    сумма нечетных чисел

---
+++ homework35/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] n = new int[size];
+FillArrayRandomNumbers(n);
+PrintArray(n);
+int sum = 0;
+
+for (int i = 0; i < n.Length; i++)
+if (n[i] % 2 == 1)
+sum += n[i];
+
+Console.WriteLine($"сумма нечетных чисел = {sum}");
+
+void FillArrayRandomNumbers(int[] n)
+{
+    for(int i = 0; i < n.Length; i++)
+    {
+        n[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] n)
+{
+    for(int i = 0; i < n.Length; i++)
+    {
+        Console.Write(n[i] + ", ");
+    }
+    Console.WriteLine();
+}
