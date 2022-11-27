157e343d7be82d08039f20dd0de976a2fdb9deb5
Olga <svey699@gmail.com>
2022-11-27 11:23:53 +0500

    сортировка массива

--- massive/Program.cs
+++ massive/Program.cs
@@ -61,17 +61,19 @@
 //PrintArray (matrix);
 
 
-int [,] pic = new int [23, 25];
-void PrintImage(int[,] image)
-{
- for (int i = 0; i < image.GetLength(0); i++)
- {
- for (int j = 0; j < image.GetLength(1); j++)
- {
- if(image[i,j] == 0) Console.Write ($" ");
- else Console.Write ($"+");
- }
- }
- }
-Console.WriteLine ();
-PrintImage (pic);
+//int [,] pic = new int [23, 25];
+//void PrintImage(int[,] image)
+//{
+ //for (int i = 0; i < image.GetLength(0); i++)
+ //{
+ //for (int j = 0; j < image.GetLength(1); j++)
+ //{
+ //if(image[i,j] == 0) Console.Write ($" ");
+ //else Console.Write ($"+");
+ //}
+ //}
+ //}
+//Console.WriteLine ();
+//PrintImage (pic);
+
+
