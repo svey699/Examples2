96d86816c662d0043285d8ea3248c9176c5ae684
Olga <svey699@gmail.com>
2022-12-02 22:46:56 +0500

    сумма натуральных чисел от M до  N

---
+++ homework66/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+using System;
+
+int  arr(int n, int m)
+{
+    int sum=0;
+    for(int i=n; i<=m; i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+int recursiveArr (int n, int m)
+{
+if(n==m)
+return m;
+else
+return n+recursiveArr(n+1, m);
+}
+Console.WriteLine(arr(1, 15));
+Console.WriteLine(recursiveArr(4, 8));
